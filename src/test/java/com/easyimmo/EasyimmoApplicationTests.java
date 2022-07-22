91437afccb12da54c443aa42c34ae57e420fb063
« Achille <achille@deribreux.be>
2022-07-22 10:57:13 +0200

    init

---
+++ src/test/java/com/easyimmo/EasyimmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyimmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyimmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
