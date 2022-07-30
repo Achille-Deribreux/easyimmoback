0c5cfdd9c6c9cf3f46679afc333813af4ee0d651
Achille Deribreux <achille@deribreux.be>
2022-07-30 20:09:46 +0200

    hotfix

--- src/main/java/com/easyimmo/reservation/controllers/ReservationController.java
+++ src/main/java/com/easyimmo/reservation/controllers/ReservationController.java
@@ -11,10 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -40,7 +37,7 @@ public class ReservationController {
      * @param id id of the wanted reservation
      * @return response entity with the wanted reservation and status code 200 if everything is ok
      */
-    @RequestMapping(value="/getById")
+    @GetMapping(value="/getById")
     public ResponseEntity<ReservationDetails> getReservationById(@RequestParam(value="id") Integer id) {
         logger.info("get request received at reservation/getById for id : {}", id);
         return new ResponseEntity<>(converter.convertToReservationDetails(reservationService.getById(id)), HttpStatus.OK);
@@ -50,7 +47,7 @@ public class ReservationController {
      * This method answer to a request at : /reservation/getAll and returns a response entity with the all the reservations
      * @return a list of all the reservations and status code 200 if everything is ok
      */
-    @RequestMapping(value="/getAll")
+    @GetMapping(value="/getAll")
     public ResponseEntity<List<ReservationSummary>> getAllReservations(
             @RequestParam(value = "propertyId",required=false)Integer propertyId,
             @RequestParam(value = "fromDate",required=false) LocalDate fromDate,
@@ -76,7 +73,7 @@ public class ReservationController {
      * @param reservationBody reservation to create
      * @return response entity with the created reservation and status code 200 if everything is ok
      */
-    @RequestMapping(value="/add")
+    @PostMapping(value="/add")
     public ResponseEntity<ReservationDetails> addReservation(@RequestBody ReservationBody reservationBody) {
         logger.info("post request received at reservation/add for reservationBody : {}", reservationBody);
         return new ResponseEntity<>(converter.convertToReservationDetails(reservationService.addReservation(converter.convertToReservationBody(reservationBody))), HttpStatus.OK);
@@ -88,7 +85,7 @@ public class ReservationController {
      * @param reservationBody reservation to update
      * @return response entity with the updated reservation and status code 200 if everything is ok
      */
-    @RequestMapping(value="/update")
+    @PutMapping(value="/update")
     public ResponseEntity<ReservationDetails> updateReservation(@RequestParam(value="id") Integer id, @RequestBody ReservationBody reservationBody) {
         logger.info("put request received at reservation/update for id : {} and reservationBody : {}", id, reservationBody);
         return new ResponseEntity<>(converter.convertToReservationDetails(reservationService.updateReservation(id, converter.convertToReservationBody(reservationBody))), HttpStatus.OK);
@@ -99,7 +96,7 @@ public class ReservationController {
      * @param id id of the reservation to delete
      * @return response entity with the deleted reservation and status code 200 if everything is ok
      */
-    @RequestMapping(value="/delete")
+    @DeleteMapping(value="/delete")
     public ResponseEntity<String> deleteReservation(@RequestParam(value="id") Integer id) {
         logger.info("delete request received at reservation/delete for id : {}", id);
         reservationService.deleteById(id);
