1c35af2963adb0430bcd70738b259bba94d40343
« Achille <achille@deribreux.be>
2022-08-02 11:59:00 +0200

    upd(fee)/EAS-23 fee service javadoc & logs

--- src/main/java/com/easyimmo/fees/service/FeeService.java
+++ src/main/java/com/easyimmo/fees/service/FeeService.java
@@ -5,6 +5,8 @@ import com.easyimmo.fees.dto.FeeCriteria;
 import com.easyimmo.fees.model.Fee;
 import com.easyimmo.fees.repository.FeeRepository;
 import com.easyimmo.fees.util.UpdateFeeHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -12,9 +14,10 @@ import java.util.List;
 
 @Service
 public class FeeService implements IFeeService{
-    //TODO logs
 
     private final FeeRepository feeRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(FeeService.class);
 
     public FeeService(FeeRepository feeRepository) {
         this.feeRepository = feeRepository;
@@ -22,16 +25,19 @@ public class FeeService implements IFeeService{
 
     @Override
     public Fee getFeeById(Integer id) {
+        logger.info("searching fee with id : {}", id);
         return feeRepository.findById(id).orElseThrow(()->new FeeNotFoundException(id.toString()));
     }
 
     @Override
     public List<Fee> getAllFees(FeeCriteria feeCriteria) {
+        logger.info("searching fees with criteria : {}", feeCriteria);
         return feeRepository.findFeeByMultipleCriteria(feeCriteria);
     }
 
     @Override
     public Fee addFee(Fee fee) {
+        logger.info("adding fee : {}", fee);
         return feeRepository.save(fee);
     }
 
@@ -39,17 +45,20 @@ public class FeeService implements IFeeService{
     public Fee updateFee(Integer id, Fee fee) {
         UpdateFeeHelper updateFeeHelper = UpdateFeeHelper.of(getFeeById(id));
         Fee updatedFee = updateFeeHelper.build(fee);
+        logger.info("updating fee : {}", updatedFee);
         return feeRepository.save(updatedFee);
     }
 
     @Override
     public void deleteById(Integer id) {
+        logger.info("deleting fee with id : {}", id);
         Fee feeToDelete = getFeeById(id);
         feeRepository.delete(feeToDelete);
     }
 
     @Override
     public Integer getTotalFeesFrom(Integer propertyId, LocalDate fromDate) {
+        logger.info("searching total fees from propertyId : {} and fromDate : {}", propertyId, fromDate);
         FeeCriteria feeCriteria = new FeeCriteria().propertyId(propertyId).minDate(fromDate);
         List<Fee> feeList =feeRepository.findFeeByMultipleCriteria(feeCriteria);
         return feeList.stream().mapToInt(Fee::getAmount).sum();
@@ -57,6 +66,7 @@ public class FeeService implements IFeeService{
 
     @Override
     public List<Fee> getLastFees(Integer propertyId, Integer nbFees) {
+        logger.info("searching last fees from propertyId : {} and nbFees : {}", propertyId, nbFees);
         FeeCriteria feeCriteria = new FeeCriteria().propertyId(propertyId).pageSize(nbFees).pageNumber(1);
         return feeRepository.findFeeByMultipleCriteria(feeCriteria);
     }
--- src/main/java/com/easyimmo/fees/service/IFeeService.java
+++ src/main/java/com/easyimmo/fees/service/IFeeService.java
@@ -7,19 +7,55 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface IFeeService {
-    //TODO Javadoc
 
+    /**
+     * Method who search a fee by id
+     * @param id the id of the fee to search
+     * @return the fee found
+     */
     Fee getFeeById(Integer id);
 
+    /**
+     * Method who search all fees with the given criteria
+     * @param feeCriteria the criteria to search
+     * @return the list of fees found
+     */
     List<Fee> getAllFees(FeeCriteria feeCriteria);
 
+    /**
+     * Method who add a fee
+     * @param fee the fee to add
+     * @return the fee added
+     */
     Fee addFee(Fee fee);
 
+    /**
+     * Method who update a fee
+     * @param id the id of the fee to update
+     * @param fee the new fee (only updated fields)
+     * @return the fee updated
+     */
     Fee updateFee(Integer id, Fee fee);
 
+    /**
+     * Method who delete a fee by id
+     * @param id the id of the fee to delete
+     */
     void deleteById(Integer id);
 
+    /**
+     * Method who search the total fees from a property
+     * @param propertyId the id of the property
+     * @param fromDate the date from which we want to search the total fees
+     * @return the total fees
+     */
     Integer getTotalFeesFrom(Integer propertyId, LocalDate fromDate);
 
+    /**
+     * Method who search the last fees from a property
+     * @param propertyId the id of the property
+     * @param nbFees the X last fees you want
+     * @return the last fees
+     */
     List<Fee> getLastFees(Integer propertyId, Integer nbFees);
 }
