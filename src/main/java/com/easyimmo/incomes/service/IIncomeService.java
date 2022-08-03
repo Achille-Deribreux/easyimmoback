f4ede727c8db38872a21527beb9e8594c880f7f4
Achille Deribreux <achille@deribreux.be>
2022-08-03 11:11:46 +0200

    fix(income)/EAS-23 add logs & javadoc

--- src/main/java/com/easyimmo/incomes/service/IIncomeService.java
+++ src/main/java/com/easyimmo/incomes/service/IIncomeService.java
@@ -8,17 +8,56 @@ import java.util.List;
 
 public interface IIncomeService {
 
+    /**
+     * This method search for all the incomes, based on a given criteria
+     * @param incomeCriteria criteria that you want to use to search for incomes
+     * @return list of all the incomes
+     */
     List<Income> getAllIncomes(IncomeCriteria incomeCriteria);
 
+    /**
+     * This method search for an income, based on a given id
+     * @param id id of the income that you search
+     * @return the wanted income
+     */
     Income getIncomeById(Integer id);
 
+    /**
+     * This method allows you to add an income
+     * @param income income that you want to add
+     * @return the added income
+     */
     Income addIncome(Income income);
 
+
+
+    /**
+     * This method allows you to update an income
+     * @param id id of the income that you want to update
+     * @param income income that you want to update
+     * @return the updated income
+     */
     Income updateIncome(Integer id,Income income);
 
+    /**
+     * This method allows you to delete an income, for a given id
+     * @param id id of the income that you want to delete
+     */
     void deleteById(Integer id);
 
+    /**
+     * This method calculates the total incomes from a given property, from a given date
+     * @param propertyId id of the property that you want to calculate the total incomes from
+     * @param fromDate date from which you want to calculate the total incomes
+     * @return the total incomes from a given property, from a given date
+     */
     Integer getTotalIncomesFrom(Integer propertyId, LocalDate fromDate);
 
+    /**
+     * This method returns the last X incomes from a given property
+     * @param propertyId id of the property that you want to get the last X incomes from
+     * @param nbIncomes number of incomes that you want to get
+     * @return the last X incomes from a given property
+     */
     List<Income> getLastIncomes(Integer propertyId, Integer nbIncomes);
 }
